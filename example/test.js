91bce416189d9546f52272cbe09738853edf5fd4
sunweibin <sunweibin1988@gmail.com>
2021-04-25 10:47:40 +0800

    更新eslint规则，升级至1.2.3

---
+++ example/test.js
@@ -0,0 +1,27 @@
+switch (key) {
+  case 1:
+    break;
+  case 2:
+    break;
+}
+
+function getXXX(a, b, c) {}
+
+getXXX(1, 2, 3);
+
+const arr = [
+  { a: 1 },
+  {
+    a: 1,
+  },
+  { a: 1 },
+  { a: 1 },
+  { a: 1 },
+];
+
+const [one, two, three, four, five] = arr;
+
+const obj = {
+  a: 1,
+  b: 2,
+};
